164e0182041cd0bf4ea8944fa8d885d5471d5c6c
dmedinas <dmedinas@gmail.com>
2024-07-20 18:47:06 -0500

    Responsive Seccion Formacion

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -4,7 +4,7 @@ import VerticalTimeline from './VerticalTimeline'
 export default function Education () {
   return (
    <section className='w-full mx-auto container mb-20 sm:max-w-2xl md:max-w-4xl'>
-    <h2 id='education' className='text-4xl flex items-center text-gray-700 font-semibold gap-4 pb-20 dark:text-white'><FaUserGraduate />Formación</h2>
+    <h2 id='education' className='text-4xl flex items-center text-gray-700 font-semibold gap-4 pb-10 dark:text-white'><FaUserGraduate />Formación</h2>
     <VerticalTimeline/>
    </section>
   )
--- src/components/TimelineItem.tsx
+++ src/components/TimelineItem.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 export default function TimelineItem ({ title, content }:{title:string, content:string}) {
   return (
     <div className="flex items-start mb-8">
-    <div className="relative flex items-center justify-center w-6 flex-shrink-0 mr-4">
-      <div className="w-3 h-3 bg-yellow-500 rounded-full z-10"></div>
+    <div className="relative hidden sm:flex items-center  justify-center w-6 flex-shrink-0 mr-4">
+      <div className="w-3 h-3  bg-yellow-500 rounded-full z-10"></div>
     </div>
-    <div className="bg-gray-200 dark:bg-gray-500 p-4 rounded drop-shadow-lg flex-grow">
+    <div className="bg-gray-200 dark:bg-gray-500 p-4 text-center sm:text-start rounded drop-shadow-lg flex-grow">
       <h3 className="font-bold text-lg text-yellow-500 mb-2">{title}</h3>
-      <p className='text-gray-700 dark:text-white leading-[1.5]'>{content}</p>
+      <p className='text-gray-700 text-sm ss:text-[16px] dark:text-white leading-[1.5]'>{content}</p>
     </div>
   </div>
   )
