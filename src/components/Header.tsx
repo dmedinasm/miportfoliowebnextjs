1d6b90ecacf232dd1ca549d545deae54c74f9067
dmedinas <dmedinas@gmail.com>
2024-08-09 23:15:00 -0500

    Mejora de UI Header en modo dark

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,7 @@ export default function Header () {
     setShowMenu(!showMenu)
   }
   return (
-    <header className={` text-[12px] xs:text-sm text-gray-700 font-medium max-w-fit m-auto  mt-2 py-2  fixed right-0 left-0 z-10 dark:text-white ${isScrolled ? 'backdrop-filter backdrop-blur-lg bg-white bg-opacity-70  px-4 xs:px-6 shadow-lg rounded-3xl' : ''}`} >
+    <header className={` text-[12px] xs:text-sm text-gray-700 font-medium max-w-fit m-auto  mt-2 py-2  fixed right-0 left-0 z-10 dark:text-white ${isScrolled ? 'backdrop-filter backdrop-blur-lg bg-white bg-opacity-70 dark:bg-gray-400 px-4 xs:px-6 shadow-lg rounded-3xl' : ''}`} >
       <nav className='flex items-center gap-2 xs:gap-4' >
         <Link href={'#education'} className='hover:text-yellow-500'>Formación</Link>
         <Link href={'#projects'} className='hover:text-yellow-500'>Proyectos</Link>
--- src/components/TimelineItem.tsx
+++ src/components/TimelineItem.tsx
@@ -6,8 +6,8 @@ export default function TimelineItem ({ title, content }:{title:string, content:
     <div className="relative hidden sm:flex items-center  justify-center w-6 flex-shrink-0 mr-4">
       <div className="w-3 h-3  bg-yellow-500 rounded-full z-10"></div>
     </div>
-    <div className="bg-gray-100 dark:bg-gray-500 p-4 text-center sm:text-start rounded drop-shadow-lg flex-grow">
-      <h3 className="font-bold text-lg text-yellow-500 mb-2">{title}</h3>
+    <div className="bg-gray-200 dark:bg-gray-500 p-4 text-center sm:text-start rounded drop-shadow-lg flex-grow">
+      <h3 className="font-bold text-lg dark:text-gray-200 text-slate-600 mb-2">{title}</h3>
       <p className='text-gray-700 text-sm ss:text-[16px] dark:text-white leading-[1.5]'>{content}</p>
     </div>
   </div>
